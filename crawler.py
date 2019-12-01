17a192bbc0feac1fb5d6ecc8c089fabf0c88014a
김현우 <hwk06023@gmail.com>
2019-12-02 04:36:00 +0900

    Crawler upload

---
+++ crawler.py
@@ -0,0 +1,17 @@
+import requests as req
+from bs4 import BeautifulSoup
+
+import re
+
+header = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Safari/605.1.15'}
+
+url = 'http://www.dbhs.co.kr/zbxe/index.php?mid=a27&document_srl=243457'
+
+page = req.get(url, headers = header)
+soup = BeautifulSoup(page.text, 'html.parser')
+divs = soup.findAll("div", {"class": "document_243457_2250 xe_content"})
+
+divs = str(divs)
+divs = re.sub('<.+?>', '', divs, 0).strip()
+
+print(divs)
